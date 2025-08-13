8a090fe61245f1b4408617c847961f32bc481a36
Gabriele Sorci <71929275+gab-25@users.noreply.github.com>
2025-08-13 19:20:37 +0000

    remove pubsub

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,6 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
-COPY service-account.json .
-
 COPY my_agent ./my_agent
 
 CMD ["python", "-m", "my_agent.main"]
--- my_agent/main.py
+++ my_agent/main.py
@@ -1,36 +1,16 @@
-import os
-import json
-from flask import Flask, request
-from google.auth import jwt
-from google.cloud import pubsub_v1
+from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
 
-service_account_info = json.load(open("service-account.json"))
-audience = "https://pubsub.googleapis.com/google.pubsub.v1.Subscriber"
-credentials = jwt.Credentials.from_service_account_info(
-    service_account_info, audience=audience
-)
-publisher = pubsub_v1.PublisherClient(credentials=credentials)
-topic_chat = publisher.topic_path(os.getenv("PROJECT_ID"), "my_agent_chat")
 
-
-@app.route("/llm_execute", methods=["GET", "POST"])
-def llm_request():
+@app.route("/chat", methods=["GET"])
+def chat():
     from .agent import execute
 
-    if request.method == "POST":
-        input_str = dict(request.json).get("input")
-        message = execute(input_str)
-        publisher.publish(
-            topic_chat,
-            json.dumps({"input": input_str, "message": message}).encode("utf-8"),
-        )
-        return ""
-
-    input_str = request.args.get("input")
-    return execute(input_str)
+    input_user = request.args.get("input")
+    output_agent = execute(input_user)
+    return jsonify({"user": input_user, "agent": output_agent}), 200
 
 
 if __name__ == "__main__":
