218bc66c5a8175a9a6016c7ccca2db8b3854b08e
gab-25 <gabrielesorci.25@gmail.com>
2026-03-07 14:32:45 +0100

    .

--- bolletta_sync/__main__.py
+++
@@ -1,6 +0,0 @@
-import uvicorn
-from bolletta_sync.main import app
-
-if __name__ == "__main__":
-    # This allows the package to be run using `python -m bolletta_sync`
-    uvicorn.run("bolletta_sync.main:app", host="0.0.0.0", port=8000, reload=False)
